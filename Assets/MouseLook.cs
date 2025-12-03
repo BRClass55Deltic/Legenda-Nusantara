37577e02f6c215b35816db0699ffada1a960fb89
LMSCoronation <daffa.a.aryasatya@gmail.com>
2025-12-03 08:41:43 +0700

    Environment

---
+++ Assets/MouseLook.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public Transform playerBody;
+    float verticalRotation = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X")  * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        verticalRotation -= mouseY;
+        verticalRotation = Mathf.Clamp(verticalRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
