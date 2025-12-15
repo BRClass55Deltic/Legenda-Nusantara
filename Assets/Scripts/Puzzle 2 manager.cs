08329a0a8fdfe3c3736da9238c68c9544b3fe97e
LMSCoronation <daffa.a.aryasatya@gmail.com>
2025-12-15 18:03:19 +0700

    LV 2 Buto Ijo Spawn

--- Assets/Scripts/Puzzle 2 manager.cs
+++ Assets/Scripts/Puzzle 2 manager.cs
@@ -18,6 +18,7 @@ public class ActivateButoIjo : MonoBehaviour
             if (butoIjo != null)
             {
                 butoIjo.SetActive(true);
+                Debug.Log("SPAWNED");
             }
         }
     }
